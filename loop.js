66df89f64c26c10260f6c7cc29eef347f92c4cc3
kazipintu <kazipintu@gmail.com>
2024-04-02 17:44:19 +0530

    MERN/eShikhon/JS/Practice-02/Loop

--- loop.js
+++ loop.js
@@ -1,7 +1,108 @@
 // for (let i = 1; i <= 100; i += 2) {
+//   // console.log(i);
+// }
+
+// for (let i = 2; i <= 100; i += 2) {
+//   // console.log(i);
+// }
+
+// // loop: It's a computer program, which executes a block of code repeatedly based on condition to start and end.
+// // It's used for repetitive automated tasks.
+// // It makes code more concise and efficient.
+
+// // while loop and for loop
+
+// // to declare while loop, we need to declare varibales generally used "let"
+
+// let sweetGiven = 0;
+// while(sweetGiven < 5){
+//   // sweetGiven = sweetGiven + 1;
+//   // sweetGiven += 1;
+//   sweetGiven++;
+//   // console.log('misti dilam');
+// }
+
+// let number = 0;
+// while(number <= 10){
+//   number++;
+//   // console.log(number);
+// }
+
+// let number = 0;
+// while(number < 10){
+//   number++;
+//   console.log(number);
+// }
+
+// let number = 0;
+// while(number < 10){
+//   console.log(number);
+//   number++;
+// }
+
+// let number = 0;
+// while(number <= 10){
+//   console.log(number);
+//   number++;
+// }
+
+// let number = 1;
+// while(number <= 10){
+//   console.log(number);
+//   number++;
+// }
+
+// let number = 1;
+// while (number <= 20) {
+//   console.log(number);
+//    // number = number + 2;
+//    number += 2;
+// }
+
+// let number = 2;
+// while (number <= 20) {
+//   console.log(number);
+//    // number = number + 2;
+//    number += 2;
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+  
+// }
+
+// index=1;
+// while (index <= 20) {
+//   console.log(index);
+//   index++;
+// }
+
+// for(let index=1; index <= 20; index++){
+//   console.log(index);
+// }
+
+// for(let i=0; i <= 20; i++){
 //   console.log(i);
 // }
 
-for (let i = 2; i <= 100; i += 2) {
-  console.log(i);
+// for(let i=1; i <= 20; i += 2){
+//   console.log(i);
+// }
+
+// for(let i=2; i <= 20; i += 2){
+//   console.log(i);
+// }
+
+const student = ['one', 'two', 'three', 'four', 'five', 'six'];
+student.unshift ('nine', 'ten');
+student.shift();
+student.shift();
+student.shift();
+// student.push('nine', 'ten');
+// console.log(student);
+
+for(let i=0; i< student.length; i++){
+  let element = student[i];
+  console.log(element);
 }
+
