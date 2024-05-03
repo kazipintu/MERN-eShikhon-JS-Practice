50e0edc48424fe5c9f77df777bf2b968858b0fc8
kazipintu <kazipintu@gmail.com>
2024-05-03 23:23:06 +0530

    MERN/JS/array/object/loop/function/math/exercise

--- exercise.js
+++ exercise.js
@@ -37,7 +37,7 @@ function findManyItem(items) {
   let newItem = [];
   for (const item of items) {
     if (newItem.indexOf(item) == -1) {
-        newItem.push(item);
+      newItem.push(item);
     }
   }
   return newItem
@@ -47,3 +47,88 @@ const manyItems = [45, null, 'germany', 'london', undefined, 'apple', 'germany',
 
 findManyItem(manyItems)
 console.log(findManyItem(manyItems));
+
+
+for (let i = 1; i <= 50; i++) {
+  //console.log(i);
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('Three & Five');
+  }
+  else if (i % 3 == 0) {
+    console.log('Three');
+  }
+  else if (i % 5 == 0) {
+    console.log('Five');
+  }
+  else if (i % 3 == 0 && i % 5 == 0) {
+    console.log('Three & Five');
+  }
+  else {
+    console.log(i);
+  }
+}
+
+/*const allPhones = [
+  { model: 'samsung', camera: '12mp', color: 'blue', price: 400 },
+  { model: 'oppo', camera: '8mp', color: 'white', price: 300 },
+  { model: 'redmi', camera: '24mp', color: 'red', price: 250 },
+  { model: 'lg', camera: '24mp', color: 'black', price: 200 },
+  { model: 'asus', camera: '12mp', color: 'blue', price: 200 },
+];
+
+
+function findCheapestPhones(phones){
+  const cheapest = [];
+  for (const phone of phones) {
+    if (phone.price < phones.price){
+        cheapest.push(phone.price);
+    }
+  }
+  return cheapest;
+
+}
+
+const findCheapestPhoneResult = findCheapestPhones(allPhones);
+console.log(findCheapestPhoneResult);*/
+
+
+function findCheapestPhones(phones) {
+  if (phones.length === 0) {
+    return []; // return an empty array if there are no phones
+  }
+
+  let cheapestPrice = phones[0].price; // Assume the first phone's price is the cheapest
+  const cheapestPhones = [phones[0]]; // Start with the first phone in the cheapestPhones array
+
+  for (let i = 1; i < phones.length; i++) {
+    const currentPrice = phones[i].price;
+
+    // If we find a phone with a cheaper price, update the cheapest price and reset the array
+    if (currentPrice < cheapestPrice) {
+      cheapestPrice = currentPrice;
+      cheapestPhones.length = 0; // Reset the array
+      cheapestPhones.push(phones[i]); // Add the current phone
+    }
+    // If the current phone has the same price as the cheapest, add it to the array
+    else if (currentPrice === cheapestPrice) {
+      cheapestPhones.push(phones[i]);
+    }
+  }
+
+  return cheapestPhones;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://github.com/aliwebdev1/Event
--- function.js
+++ function.js
@@ -2,68 +2,68 @@
 // 1. Function Creation, 2. Function starting, 3. call, 4. Function Return (call)
 
 function startFan() {
-//   console.log('function is running');
+console.log('function is running');
 }
 
 startFan()
 
 function takeShower(people){
-//   console.log('Now in shower', people);
+console.log('Now in shower', people);
 }
 takeShower('Shorif bhai')
 
 function name(params) {
-//   console.log(params, 'parameters');
+console.log(params, 'parameters');
 }
 name('number')
 
 function startTheFan(){ //function starting (parameter, lika a variable)
-  //console.log('Fan is started'); //rerturning
+console.log('Fan is started'); //rerturning
 }
 startTheFan()  //calling (input)
 
 function takeShower(people){
-  //console.log(people, 'now in shower');
+console.log(people, 'now in shower');
 }
 takeShower('Ali Bhaiya')
 
 function takeShower(people){
-  //console.log(people);
+console.log(people);
 }
 takeShower('Ali Bhaiya')
 
 function takeShower(people, group){
-  //console.log();
+console.log();
 }
 takeShower(101, 'who')
 
 function addTwoNumbers(num1, num2){
   const total = num1 + num2;
-  //console.log(total);
+  console.log(total);
 }
 addTwoNumbers(5, 10)
 addTwoNumbers(15, 10)
 
 function addTwoNumbers(num1, num2){
   const total = num1 + num2;
-  // console.log(total);
+  console.log(total);
   return total;
 }
 
-const dadi = addTwoNumbers(12, 15);
-// console.log(dadi);
+const dadi2 = addTwoNumbers(12, 15);
+console.log(dadi2);
 
 function addTwoNumbers(num1, num2){
   const total = num1 + num2;
-  // console.log(total);
+  console.log(total);
   return total;
  
 }
 
-const dadi = addTwoNumbers(12, 15);
-// console.log(dadi);
-const baba = addTwoNumbers(18, 20);
-//console.log(baba);
+const dadi1 = addTwoNumbers(12, 15);
+console.log(dadi1);
+const baba1 = addTwoNumbers(18, 20);
+console.log(baba1);
 
 
 function blenderJuicer(l1, p1, s1, w1){
@@ -74,15 +74,15 @@ function blenderJuicer(l1, p1, s1, w1){
 const kabab = 'beefpaste'
 const filler = 'besan'
 const customer = blenderJuicer('lemon', 'pineapple', 'salt', 'water');
-// console.log(customer);
-const maa = blenderJuicer('onion', 'ginger', 'garlic', 'salt');
-// console.log(maa + kabab + filler);
+console.log(customer);
+const maa1 = blenderJuicer('onion', 'ginger', 'garlic', 'salt');
+console.log(maa1 + kabab + filler);
 
 // object declaration
 
-const student = { class:10, section:'D', roll:53, mathMarks:48, fitBody: false, behaveGood:'true', phone:undefined}
+const student2 = { class:10, section:'D', roll:53, mathMarks:48, fitBody: false, behaveGood:'true', phone:undefined}
 
-const laptop = {brand:'framework', nanoCircuit: undefined, color: 'aluminium', price: 1200, availableIndia: undefined}
+const laptop2 = {brand:'framework', nanoCircuit: undefined, color: 'aluminium', price: 1200, availableIndia: undefined}
 
 // array in third bracket [], object always in second/curly bracket {}
 
@@ -95,29 +95,29 @@ const laptop = {brand:'framework', nanoCircuit: undefined, color: 'aluminium', p
 
 const laptop = {brand:'framework', nanoCircuit: undefined, body: 'aluminium', color: 'silver', price: 1200, availableIndia: undefined}
 
-// console.log(laptop.color);
-//console.log(laptop);
+console.log(laptop.color);
+console.log(laptop);
 laptop.price = 900;
-//console.log(laptop);
+console.log(laptop);
 
-// object data_type is "non=-primitive" data type
+//object data_type is "non=-primitive" data type
 
 laptop.price = '$900';
-//console.log(laptop);
+console.log(laptop);
 laptop['price'] = '$600'
-//console.log(laptop);
+console.log(laptop);
 const priceKey = 'price'
 laptop[priceKey] = '$1000'
-//console.log(laptop);
+console.log(laptop);
 
 const student = { class:10, section:'D', roll:53, mathMarks:48, fitBody: false, behaveGood:'true', address:undefined}
 
 const newObjectPhone = 'phone'
 student.phone = 987654321
 student['phone'] = 987654321
-//console.log(student);
+console.log(student);
 delete student.behaveGood;
-//console.log(student);
+console.log(student);
 
 function whetherEvenOrOdd(numb) {
   if (numb == 0) {
@@ -132,22 +132,22 @@ function whetherEvenOrOdd(numb) {
 }
 
 const mama = whetherEvenOrOdd(-325)
-// console.log(mama);
+console.log(mama);
 
 const result = whetherEvenOrOdd(5624)
-// console.log(result);
+console.log(result);
 
 const maa = whetherEvenOrOdd(15264)
-// console.log(maa);
+console.log(maa);
 
 const baba = whetherEvenOrOdd(15635)
-// console.log(baba);
+console.log(baba);
 
 const vai = whetherEvenOrOdd(0)
-// console.log(vai);
+console.log(vai);
 
 const apu = whetherEvenOrOdd(-35)
-// console.log(vai);
+console.log(vai);
 
 function isLeapYear(year){
   if((year > 0 && year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0) ){
@@ -157,22 +157,22 @@ function isLeapYear(year){
 }
 
 const time = isLeapYear (1300);
-// console.log(time);
+console.log(time);
 
 const asif = isLeapYear(1500);
-// console.log(asif);
+console.log(asif);
 
 factorial
 
-3! = 3 * 2 * 1
-4!= 4 * 3 * 2 * 1
-5! = 5 * 4 * 3 * 2 * 1
+//3! = 3 * 2 * 1
+//4!= 4 * 3 * 2 * 1
+//5! = 5 * 4 * 3 * 2 * 1
 
 let factorial = 1;
 for(let i = 1; i <= 7; i ++ ){
   factorial = factorial * i;
 }
-//console.log(factorial); */
+console.log(factorial);
 
 function calculateFactorial(number){
   let factorial = 1;
@@ -196,26 +196,26 @@ function determineFactorial(number) {
 }
 
 const chand = determineFactorial(8);
-//console.log(chand);
+console.log(chand);
 
 function inchesToFeet(inches){
   const feet = inches/12;
   return feet
 }
 
-const result = inchesToFeet(2345);
-// console.log(result.toFixed(2));
+const result10 = inchesToFeet(2345);
+console.log(result10.toFixed(2));
 
-const maa = inchesToFeet(1402);
-// console.log(maa.toFixed(3));
+const maa2 = inchesToFeet(1402);
+console.log(maa2.toFixed(3));
 
-const baba = inchesToFeet(1305);
-// console.log(baba.toFixed(2));
+const baba2 = inchesToFeet(1305);
+console.log(baba2.toFixed(2));
 
-const vai = inchesToFeet(5201);
-// console.log(parseFloat(vai));
+const vai2 = inchesToFeet(5201);
+console.log(parseFloat(vai2));
 
-const apu = inchesToFeet(76054);
-// console.log(parseInt(apu));
+const apu2 = inchesToFeet(76054);
+console.log(parseInt(apu2));
 
 
